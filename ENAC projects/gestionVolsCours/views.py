a855f286086088ae2b72939cc2c2d4cda3878be0
SeaCaulk <super4xe@gmail.com>
2025-02-05 10:00:33 +0100

    plane interface done

--- ENAC projects/gestionVolsCours/views.py
+++ ENAC projects/gestionVolsCours/views.py
@@ -9,13 +9,25 @@ def load():
   else:
     flights = []
   return(flights)
+def laod():
+  if os.path.getsize("ENAC projects/gestionVolsCours/aircrafts.txt")>0:
+    with open("ENAC projects/gestionVolsCours/aircrafts.txt", "r") as file:
+        aircrafts = json.load(file)
+  else:
+    aircrafts = []
+  return(aircrafts)
 def dump(l):
   flights=load()
   flights.append(l)
   with open("ENAC projects/gestionVolsCours/flights.txt", "w") as file:
     json.dump(flights,file)
-app = Flask(__name__)
+def dmup(l):
+  flights=laod()
+  flights.append(l)
+  with open("ENAC projects/gestionVolsCours/aircrafts.txt", "w") as file:
+    json.dump(flights,file)
 
+app = Flask(__name__)
 app.template_folder = "template"
 app.static_folder = "static"
 app.config.from_object('config')
@@ -31,6 +43,28 @@ def ajouter():
     dump([request.form["indicatif"], request.form["OACIDep"], request.form["OACIArr"], request.form["Hdep"], request.form["Harr"]])
   return render_template("ajouter.html", flights=load())
 
+@app.route("/manage",methods=['POST','GET'])
+def manage():
+  if request.method=="POST":
+    dmup([request.form["indicatif"], request.form["Type"], request.form["Temps"]])
+  return render_template("manage.html", aircrafts=laod(), current=-1)
+@app.route("/manage<w>[<int:id>]", methods=['POST','GET'])
+def manageId(w,id):
+  if request.method == "POST":
+    aircrafts=laod()
+    aircrafts[int(request.form["current"])]=[request.form["indicatif"], request.form["Type"], request.form["Temps"]]
+    with open("ENAC projects/gestionVolsCours/aircrafts.txt", "w") as file:
+      json.dump(aircrafts,file)
+    return render_template("manage.html", aircrafts=laod(), current=-1, w='M')
+  if w=='M':
+    return render_template("manage.html", aircrafts=laod(), current=id, w='M')
+  elif w=='S':
+    aircrafts=laod()
+    del aircrafts[id]
+    with open("ENAC projects/gestionVolsCours/aircrafts.txt",'w') as file:
+      json.dump(aircrafts,file)
+    return render_template("manage.html", aircrafts=laod(), current=-1, w='S')
+
 @app.route("/modifier", methods=["POST","GET"])
 def modifier():
   if request.method=="POST":
@@ -40,6 +74,7 @@ def modifier():
     with open("ENAC projects/gestionVolsCours/flights.txt", "w") as file:
       json.dump(flights,file)
   return render_template("modifier.html", flights=load(), current=-1)
+
 @app.route("/modifier[<int:id>]")
 def modifId(id):
   return render_template("modifier.html", flights=load(), current=id)
@@ -47,6 +82,7 @@ def modifId(id):
 @app.route("/supprimer")
 def supprimer():
   return render_template("supprimer.html", flights=load())
+
 @app.route("/supprimer[<int:id>]")
 def suppId(id):
   flights=load()
